de38f0c7fffe4109121dda88de3e38741efb5f2e
Chantal Justamond <cjustamm66@me.com>
2017-05-20 12:57:46 -0600

    user can create a category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added"
+      redirect_to category_path(@category)
+    else
+      flash[:alert] = 'That category already exists'
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+end
